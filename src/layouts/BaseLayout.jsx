81b8ccce203c7009d95c2eed51dc837aae2d70f3
Gerard <gerpincas@gmail.com>
2024-09-29 12:20:34 +0200

    Hash routing & very basic layout

---
+++ src/layouts/BaseLayout.jsx
@@ -0,0 +1,26 @@
+import {Link} from "react-router-dom";
+import './BaseLayout.css'
+
+function BaseLayout({ children }) {
+    return (
+        <div>
+            <header>
+                <nav>
+                    <ul>
+                        <li><Link to="/">Home</Link></li>
+                        <li><Link to="/gallery">Gallery</Link></li>
+                        <li><Link to="/meshEditor">Mesh Editor</Link></li>
+                    </ul>
+                </nav>
+            </header>
+            <main>
+                { children }
+            </main>
+            <footer>
+                <p>© 2024 Proto</p>
+            </footer>
+        </div>
+    )
+}
+
+export default BaseLayout;
---
+++ src/pages/MeshEditor/MeshEditor.jsx
@@ -0,0 +1,7 @@
+function MeshEditor() {
+    return (
+        <p>MeshEditor!</p>
+    )
+}
+
+export default MeshEditor;
