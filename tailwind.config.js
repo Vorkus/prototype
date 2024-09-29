4ddf39b82992f7be938495cabcd7046d69120bd9
Gerard <gerpincas@gmail.com>
2024-09-29 20:26:31 +0200

    Adding dependecies (react libraries for tailwind) && adding "Inter var" font style

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,15 @@
-/** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 export default {
     content: [
         "./index.html",
         "./src/**/*.{js,jsx,ts,tsx}"],
     theme: {
-        extend: {},
+        extend: {
+            fontFamily: {
+                sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+            },
+        },
     },
     plugins: [],
 }
