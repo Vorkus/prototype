9a25e36d25449a495adb4486d8ae9ca6f322fa90
Gerard <gerpincas@gmail.com>
2024-10-01 22:25:46 +0200

    Hotfix spacing

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,19 +6,19 @@ export default {
     extend: {
       backgroundImage: {
         "main-gradient": `
-                    linear-gradient(
-                        to top right,
-                        ${colors.purple["400"]},
-                        ${colors.violet["400"]},
-                        ${colors.indigo["400"]},
-                        ${colors.blue["400"]} ,
-                        ${colors.sky["400"]},
-                        ${colors.cyan["400"]},
-                        ${colors.teal["400"]},
-                        ${colors.emerald["400"]},
-                        ${colors.green["400"]},
-                        ${colors.lime["400"]}
-                    )`,
+          linear-gradient(
+            to top right,
+            ${colors.purple["400"]},
+            ${colors.violet["400"]},
+            ${colors.indigo["400"]},
+            ${colors.blue["400"]} ,
+            ${colors.sky["400"]},
+            ${colors.cyan["400"]},
+            ${colors.teal["400"]},
+            ${colors.emerald["400"]},
+            ${colors.green["400"]},
+            ${colors.lime["400"]}
+          )`,
       },
     },
   },
