5ba968267e8ce7acfdc15a41e6ed6ab66edc20bd
Gerard <gerpincas@gmail.com>
2024-10-01 21:35:29 +0200

    Removing inter as font and adding prettier

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,10 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
-
 export default {
     content: [
         "./index.html",
-        "./src/**/*.{js,jsx,ts,tsx}"],
+        "./src/**/*.{js,jsx,ts,tsx}"
+    ],
     theme: {
-        extend: {
-            fontFamily: {
-                sans: ['Inter var', ...defaultTheme.fontFamily.sans],
-            },
-        },
+        extend: {},
     },
     plugins: [],
 }
