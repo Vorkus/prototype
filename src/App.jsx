229d529d697ab92789a7d9f321d502faff94c2e7
Gerard <gerpincas@gmail.com>
2024-10-02 21:23:42 +0200

    Correct indentation

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
 import BaseLayout from "./layouts/BaseLayout.jsx";
-import {Route, Routes} from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home.jsx";
 import Gallery from "./pages/Gallery/Gallery.jsx";
 import MeshEditor from "./pages/MeshEditor/MeshEditor.jsx";
 
 function App() {
-    return (
-        <BaseLayout>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/gallery" element={<Gallery />} />
-                <Route path="/meshEditor" element={<MeshEditor />} />
-            </Routes>
-        </BaseLayout>
-    );
+  return (
+    <BaseLayout>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/gallery" element={<Gallery />} />
+        <Route path="/meshEditor" element={<MeshEditor />} />
+      </Routes>
+    </BaseLayout>
+  );
 }
 
-export default App
+export default App;
--- src/layouts/BaseLayout.jsx
+++ src/layouts/BaseLayout.jsx
@@ -2,15 +2,13 @@ import Header from "../components/Header/Header.jsx";
 import Footer from "../components/Footer/Footer.jsx";
 
 function BaseLayout({ children }) {
-    return (
-        <>
-            <Header />
-            <main>
-                { children }
-            </main>
-            <Footer />
-        </>
-    )
+  return (
+    <>
+      <Header />
+      <main>{children}</main>
+      <Footer />
+    </>
+  );
 }
 
 export default BaseLayout;
